d24705f227fbbefbba0a4301e96f191f2a3c0bc5
Callum Burns <cp19cb97@hotmail.com>
2015-08-23 18:21:15 +0100

    Add woofing support

--- woofbot.c
+++ woofbot.c
@@ -13,7 +13,9 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <libircclient.h>
 #include <libirc_rfcnumeric.h>
@@ -21,23 +23,67 @@
 
 static irc_callbacks_t callbacks;
 
-static void report_in(irc_session_t *session, const char *channel,
-        const char *input);
+typedef void command_func(irc_session_t *session, const char *nick,
+        const char *channel, const char *input);
+
+static command_func report_in;
+static command_func woof;
+
+#define WOOF_COUNT 8
+
+static const char *woofs[WOOF_COUNT] = {
+    "Woof!",
+    "W-woof!",
+    "Woof!  Woof!",
+    "Grrrr!",
+    "W-woof...  :3c",
+    "Rrruff!",
+    "Yip!",
+    "Arf!"
+};
 
 static const char *command_names[] = {
     "bots",
+    "woof",
     NULL
 };
 
-static void (*command_funcs[])(irc_session_t *, const char *, const char *) = {
+static command_func *command_funcs[] = {
     report_in,
+    woof,
     NULL
 };
 
-static void report_in(irc_session_t *session, const char *channel,
-        const char *input)
+static void report_in(irc_session_t *session, const char *nick,
+        const char *channel, const char *input)
 {
     irc_cmd_msg(session, channel, "Reporting in! [C]");
+}
+
+static void woof(irc_session_t *session, const char *nick,
+        const char *channel, const char *input)
+{
+    char msg[256];
+    const char *woofmsg;
+    const char *name;
+
+    if (input) {
+        name = input;
+    } else {
+        name = nick;
+    }
+
+    woofmsg = woofs[rand() % WOOF_COUNT];
+
+    if (strlen(name) + strlen(woofmsg) + 2 > 255) {
+        return;
+    }
+
+    strcpy(msg, name);
+    strcat(msg, ": ");
+    strcat(msg, woofmsg);
+
+    irc_cmd_msg(session, channel, msg);
 }
 
 static void event_connect(irc_session_t *session, const char *event,
@@ -77,7 +123,18 @@ static void event_channel(irc_session_t *session, const char *event,
         }
 
         if (!notfound) {
-            command_funcs[i](session, params[0], strchr(params[1], ' ') + 1);
+            char *input = strchr(params[1], ' ');
+
+            if (input) {
+                input++;
+                printf("%s sent command \"%s\" with input \"%s\".\n",
+                        origin, commandstr, input);
+            } else {
+                printf("%s sent command \"%s\" with no input.\n",
+                        origin, commandstr);
+            }
+
+            command_funcs[i](session, origin, params[0], input);
         }
     }
 }
@@ -95,6 +152,8 @@ static void event_ctcp_req(irc_session_t *session, const char *event,
 int main(void)
 {
     irc_session_t *session;
+
+    srand(time(NULL));
 
     callbacks.event_connect = event_connect;
     callbacks.event_numeric = event_numeric;
